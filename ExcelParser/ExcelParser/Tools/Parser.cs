906849ef50af9284359064425018c475e0cd370f
Dmitri <lipov758@gmail.com>
2019-12-20 17:38:49 +0300

    fix

--- ExcelParser/ExcelParser/Tools/Parser.cs
+++ ExcelParser/ExcelParser/Tools/Parser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Windows;
 using Spire.Xls;
 using Spire.Xls.Core;
 
@@ -26,13 +28,18 @@ namespace ExcelParser.Tools
 
 
             var finalBook = new Workbook();
-            finalBook.LoadFromFile("../../../EmptyTable.xlsx");
+            try
+            {
+                finalBook.LoadFromFile("EmptyTable.xlsx");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не найден фаил с пустой таблицей!");
+            }
 
             var finalSheet = finalBook.Worksheets[0];
             var newBook = new Workbook();
             var columnCount = finalBook.Worksheets[0].Columns.Length;
-//            var emptyrow = finalSheet.Columns.FirstOrDefault(p => p.Value.ToLower() == "итого");
-
 
             var source = finalSheet.Range[finalSheet.Columns[0].Row, 1, finalSheet.Columns[0].Row, columnCount];
             var dest = newBook.Worksheets[0].Range[i, 1, i, columnCount];
@@ -47,16 +54,19 @@ namespace ExcelParser.Tools
                 {
                     var worksheet = (Worksheet) value;
                     //Loop through cells
-                    foreach (CellRange cell in worksheet.Range)
+                    foreach (var xlsRange in worksheet.Range)
                     {
+                        var cell = (CellRange) xlsRange;
+                        xlsRange.BorderAround(LineStyleType.Medium, Color.Black);
                         //If the cell contain formula, get the formula value, clear cell content, and then fill the formula value into the cell 
                         if (cell.HasFormula)
                         {
-                            Object values = cell.FormulaValue;                        
+                            Object values = cell.FormulaValue;
                             cell.Clear(ExcelClearOptions.ClearContent);
                             cell.Value2 = values;
                         }
                     }
+
                     newBook.Worksheets[0].Copy(worksheet.Range[currentSheet.Row, 4, currentSheet.Row, 32],
                         newBook.Worksheets[0].Range[$"D{i}:AI{i}"], true);
                 }
@@ -70,39 +80,32 @@ namespace ExcelParser.Tools
             }
 
             newBook.ActiveSheetIndex = 0;
-//            var rowCount = newBook.Worksheets[0].Rows.Length;
-//            var destiny = newBook.Worksheets[0].Range[i, 1, i, columnCount];
-            var sourcesheet = finalSheet.Range[$"A21:AI27"];
-            var desty = newBook.Worksheets[0].Range[$"A{(Worksheets.Count+8)}:AI{Worksheets.Count+8}"];
-            finalSheet.Copy(sourcesheet, desty, true);
-//            finalSheet.Copy(emptyrow, dest, true);
-            //newBook.Worksheets[0].Range["B" + (Worksheets.Count+2)].Value = "Итого";
-//            var colums = newBook.Worksheets[0].Columns;
-//            var cells = newBook.Worksheets[0].Columns[4].Cells;
 
-//            var bbb = new Dictionary<int, int>();
-//            foreach (var cell in cells.Where(p => !p.IsBlank).Skip(1))
-//            {
-//                bbb.Add(cell.Column, Convert.ToInt32(cell.DisplayedText));
-//            }
+            var sourceSheet = finalSheet.Range[$"A21:AI27"];
+            var desty = newBook.Worksheets[0].Range[$"A{(Worksheets.Count + 8)}:AI{Worksheets.Count + 8}"];
+            finalSheet.Copy(sourceSheet, desty, true);
+
             for (int k = newBook.Worksheets[0].Rows.Count() - 1; k >= 0; k--)
             {
                 if (newBook.Worksheets[0].Rows[k].IsBlank)
                 {
-                    newBook.Worksheets[0].DeleteRow(k+1);
+                    newBook.Worksheets[0].DeleteRow(k + 1);
                 }
             }
 
-            var finalCells =  newBook.Worksheets[0].Range[$"A{(Worksheets.Count+2)}:AI{Worksheets.Count+2}"];;
+            var finalCells = newBook.Worksheets[0].Range[$"A{(Worksheets.Count + 2)}:AI{Worksheets.Count + 2}"];
+            ;
             foreach (var cell in finalCells.Skip(3))
             {
                 cell.Value = newBook.Worksheets[0]
-                    .Columns[cell.Column-1]
-                    .Cells.Where(p=> p.DisplayedText != string.Empty)
+                    .Columns[cell.Column - 1]
+                    .Cells.Where(p => p.DisplayedText != string.Empty)
                     .Skip(1)
                     .Sum(p => Convert.ToInt32(p.DisplayedText)).ToString();
             }
 
+            newBook.Worksheets[0].Range[$"D2:AI{Worksheets.Count + 2}"].BorderAround(LineStyleType.Medium, Color.Black);
+            newBook.Worksheets[0].Range[$"D2:AI{Worksheets.Count + 2}"].BorderInside(LineStyleType.Medium, Color.Black);
             return newBook;
         }
     }
